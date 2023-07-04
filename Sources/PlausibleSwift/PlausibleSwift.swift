8b9bd6298c2ac62bda1c5869db8ff7dfe0c3162b
Nick O'Neill <nick.oneill@gmail.com>
2023-07-03 17:47:14 -0700

    add regular event tracking too

--- Sources/PlausibleSwift/PlausibleSwift.swift
+++ Sources/PlausibleSwift/PlausibleSwift.swift
@@ -1,12 +1,13 @@
 import Foundation
 
-/// PlausibleSwift is a
+/// PlausibleSwift is an implementation of the Plausible Analytics REST events API as described here: https://plausible.io/docs/events-api
 public struct PlausibleSwift {
     public private(set) var domain = ""
     
     private let PlausibleAPIEventURL = URL(string: "https://plausible.io/api/event")!
 
     /// Initializes a plausible object used for sending events to Plausible.io
+    /// Throws a `invalidDomain` error if the domain you pass cannot be turned into a URL
     /// - Parameters:
     ///     - domain: a fully qualified domain representing a site you have set up on plausible.io, such as `5calls.org`
     public init(domain: String) throws {
@@ -18,19 +19,38 @@ public struct PlausibleSwift {
         self.domain = domain
     }
     
-    /// Sends a pageview event...
+    /// Sends a pageview event to Plausible for the specified path
+    /// - Parameters:
+    ///     - path: a URL path to use as the pageview location (as if it was viewed on a website). There doesn't have to be anything served at this URL.
     /// Throws a `domainNotSet` error if it has been configured with an empty domain
-    ///
     public func trackPageview(path: String) throws {
         guard self.domain != "" else {
             throw PlausibleError.domainNotSet
         }
         
+        plausibleRequest(name: "pageview", path: path)
+    }
+
+    /// Sends a named event to Plausible for the specified path
+    /// - Parameters:
+    ///     - event: an arbitrary event name for your analytics.
+    ///     - path: a URL path to use as the pageview location (as if it was viewed on a website). There doesn't have to be anything served at this URL.
+    /// Throws a `domainNotSet` error if it has been configured with an empty domain.
+    /// Throws a `eventIsPageview` error if you try to specific the event name as `pageview` which may indicate that you're holding it wrong.
+    public func trackEvent(event: String, path: String) throws {
+        guard event != "pageview" else {
+            throw PlausibleError.eventIsPageview
+        }
+        
+        plausibleRequest(name: event, path: path)
+    }
+    
+    private func plausibleRequest(name: String, path: String) {
         var req = URLRequest(url: PlausibleAPIEventURL)
         req.httpMethod = "POST"
         req.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        let jsonData = try? JSONSerialization.data(withJSONObject: ["name":"pageview","url": constructPageviewURL(path: path),"domain": domain])
+        let jsonData = try? JSONSerialization.data(withJSONObject: ["name": name,"url": constructPageviewURL(path: path),"domain": domain])
         req.httpBody = jsonData
         
         URLSession.shared.dataTask(with: req) { data, response, err in
@@ -55,4 +75,5 @@ public struct PlausibleSwift {
 public enum PlausibleError: Error {
     case domainNotSet
     case invalidDomain
+    case eventIsPageview
 }
